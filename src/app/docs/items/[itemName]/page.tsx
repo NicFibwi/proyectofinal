46de750eedf3cc9113a1abd6741bfa62c48a3147
NicFibwi <snicolai@fibwi.com>
2025-04-07 12:20:56 +0200

    fix lint x2

--- src/app/docs/items/[itemName]/page.tsx
+++ src/app/docs/items/[itemName]/page.tsx
@@ -1,15 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { use } from "react"; // Import the `use` hook to handle promises
 import React from "react";
 import ItemDetailCard from "~/components/item-detail-card";
 import { Card } from "~/components/ui/card";
 import type { ItemInfo } from "~/types/types";
 
 export default function ItemDetailPage({
-  params,
+  params: paramsPromise,
 }: {
-  params: { itemName: string };
+  params: Promise<{ itemName: string }>;
 }) {
-  const { itemName } = params; // Access the itemName directly from params
+  const { itemName } = use(paramsPromise); // Resolve the promise using `use`
 
   return <ItemDetailCard name={itemName} />;
 }
